d18e39174f78771053ded9f3535c41221a6e782d
Kelly Klabunde <kelly.klabunde@gmail.com>
2022-05-18 13:35:01 +0200

    added bootstrap, initial CardBootstrap.vue created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <FirstPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstPage from './components/FirstPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstPage
   }
 }
 </script>
---
+++ src/components/FirstPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello Main</h1>
+    <CardBootstrap />
+  </div>
+</template>
+
+<script>
+import CardBootstrap from '../card/CardBootstrap.vue'
+
+export default {
+  name: 'FirstPage',
+  components: {
+    CardBootstrap,
+  }
+}
+</script>
